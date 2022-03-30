a6fcb72e262a37f215a7595d3a7a8e5f4a81c22e
zeyadomran <ziomran@gmail.com>
2022-03-29 18:11:06 -0600

    Created Working Login & Signup

---
+++ CPSC481/User.cs
@@ -0,0 +1,14 @@
+public class User
+{
+	public string firstName;
+	public string lastName;
+	public string email;
+	public string password;
+	public User(string firstName, string lastName, string email, string password)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+}
